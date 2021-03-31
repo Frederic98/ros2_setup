d4dbaad9a9aebc6737b687d41e72e203b068183a
Frederic Meeuwissen <frederic.meeuwissen@gmail.com>
2021-03-31 23:46:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ros2setup",
+    version="1.0",
+    author="Frederic",
+    description="Python library for the ROS2 module setup",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Frederic98/ros2_setup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5'
+)
